593c450a464df1f664aad06e09c3d58abef06952
RedTea <64125023+redteadeveloper@users.noreply.github.com>
2020-10-18 06:37:20 +0900

    Added semicolons

--- SandBox/index.js
+++ SandBox/index.js
@@ -1,20 +1,20 @@
 var antlr4 = require('antlr4');
-var fs = require('fs')
+var fs = require('fs');
 
 var MyGrammarLexer = require('./sandboxLexer.js').sandboxLexer;
 var MyGrammarParser = require('./sandboxParser.js').sandboxParser;
 var customVisitor = require('./customVisitor.js').customVisitor;
 
-console.log("\n### SandBox Programming Language 0.1.0 ###\n")
-console.log("======================")
-console.log("RESULT:")
-console.log("----------------------")
+console.log("\n### SandBox Programming Language 0.1.0 ###\n");
+console.log("======================");
+console.log("RESULT:");
+console.log("----------------------");
 
 var input = fs.readFileSync("./snbx-files/main.snbx").toString();
 
 var chars = new antlr4.InputStream(input);
 var lexer = new MyGrammarLexer(chars);
-lexer.removeErrorListeners()
+lexer.removeErrorListeners();
 
 var tokens  = new antlr4.CommonTokenStream(lexer);
 var parser = new MyGrammarParser(tokens);
@@ -22,7 +22,7 @@ parser.buildParseTrees = true;
 
 var tree = parser.program();
 
-var visitor = new customVisitor()
-visitor.visit(tree)
+var visitor = new customVisitor();
+visitor.visit(tree);
 
-console.log("======================\n")
+console.log("======================\n");
