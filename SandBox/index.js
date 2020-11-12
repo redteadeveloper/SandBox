452b3a900399e4c8380a026b02c2ecac87650f56
unknown <myadiuio@gmail.com>
2020-11-12 14:39:20 +0900

    Version 0.1.3 update

--- SandBox/index.js
+++ SandBox/index.js
@@ -1,27 +1,48 @@
 var antlr4 = require("antlr4");
 var fs = require("fs");
+var readline = require('readline')
 
-var MyGrammarLexer = require('./sandboxLexer.js').sandboxLexer;
-var MyGrammarParser = require('./sandboxParser.js').sandboxParser;
-var customVisitor = require('./customVisitor.js').customVisitor;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-console.log("\n### SandBox Programming Language 0.1.2 ###\n");
-console.log("======================");
-console.log("RESULT:");
-console.log("----------------------");
+rl.question("Enter .snbx file path: ", function(path) {
 
-var input = fs.readFileSync("./snbx-files/main.snbx").toString();
+    if (!path.endsWith(".snbx")) {
+        return console.log("Not a .snbx file. Press Ctrl + c to exit.")
+    }
 
-var chars = new antlr4.InputStream(input);
-var lexer = new MyGrammarLexer(chars);
-var tokens  = new antlr4.CommonTokenStream(lexer);
-var parser = new MyGrammarParser(tokens);
-var visitor = new customVisitor();
+    let filepath = path
 
-lexer.removeErrorListeners();
-parser.buildParseTrees = true;
+    var MyGrammarLexer = require('./sandboxLexer.js').sandboxLexer;
+    var MyGrammarParser = require('./sandboxParser.js').sandboxParser;
+    var customVisitor = require('./customVisitor.js').customVisitor;
 
-var tree = parser.program();
-visitor.visit(tree);
+    try {
+        var input = fs.readFileSync(filepath).toString();
+    } catch {
+        return console.log("Couldn't find file. Press Ctrl + c to exit.")
+    }
 
-console.log("======================\n");
+    console.log("\n### SandBox Programming Language 0.1.2 ###\n");
+    console.log("======================");
+    console.log("RESULT:");
+    console.log("----------------------");
+
+    var chars = new antlr4.InputStream(input);
+    var lexer = new MyGrammarLexer(chars);
+    var tokens  = new antlr4.CommonTokenStream(lexer);
+    var parser = new MyGrammarParser(tokens);
+    var visitor = new customVisitor();
+
+    lexer.removeErrorListeners();
+    parser.buildParseTrees = true;
+
+    var tree = parser.program();
+    visitor.visit(tree);
+
+    console.log("======================\n");
+    console.log('Complete. Press Ctrl + c to exit.');
+
+});
